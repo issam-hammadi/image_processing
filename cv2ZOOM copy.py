e8ae3824890bbc36f03ed990f558addbeec13d7b
issam Hammadi <issamhammadi32@gmail.com>
2024-02-07 12:19:45 +0100

    added a zoom function

--- cv2ZOOM copy.py
+++ cv2ZOOM copy.py
@@ -2,6 +2,14 @@ import cv2
 """There is unfortunately no way in order to make the openCv window with the image bigger, so a code is needed 
 to show only a zoomed part in the image """
 
+
+def zoom_at(img, zoom=1, angle=0, coord=None):
+    cy, cx = [ i/2 for i in img.shape[:-1] ] if coord is None else coord[::-1]
+
+    rot_mat = cv2.getRotationMatrix2D((cx,cy), angle, zoom)
+    result = cv2.warpAffine(img, rot_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
+    
 # Read the image 
 image = cv2.imread(r"C:\Users\issam.hammadi\OneDrive - IRT M2P\Bureau\Stage IRT M2P\code\images_echantillon_321\smallest image.jpg")
 
